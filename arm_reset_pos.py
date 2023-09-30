0f9d27fbb0ca33ebe4cd8b07b10561a7585b6988
brad-mombie <brad.momberg@gmail.com>
2023-09-29 19:34:58 -0700

    added a reset

--- arm_reset_pos.py
+++ arm_reset_pos.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 from Arm_Lib import Arm_Device
 
@@ -14,8 +15,13 @@ print("servo_5: ", arm.Arm_serial_servo_read(5))
 print("servo_6: ", arm.Arm_serial_servo_read(6))
 
 arm.Arm_serial_servo_write(1, 92, 500)
+time.sleep(1)
 arm.Arm_serial_servo_write(2, 114, 500)
+time.sleep(1)
 arm.Arm_serial_servo_write(3, 26, 500)
+time.sleep(1)
 arm.Arm_serial_servo_write(4, 29, 500)
+time.sleep(1)
 arm.Arm_serial_servo_write(5, 89, 500)
+time.sleep(1)
 
