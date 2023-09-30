c261a841f52a13f9eac50af2d9460090766cfd50
brad-mombie <brad.momberg@gmail.com>
2023-09-29 19:12:47 -0700

    Updated to green block

--- cv2_cam_test.py
+++ cv2_cam_test.py
@@ -6,9 +6,9 @@ from Arm_Lib import Arm_Device
 arm = Arm_Device()
 
 # Initialize the current angles of the servos
-current_angle_of_servo_1 = arm.Arm_serial_servo_read(1) or 90  # Default to 90 if None
-current_angle_of_servo_2 = arm.Arm_serial_servo_read(2) or 90  # Default to 90 if None
-current_angle_of_servo_3 = arm.Arm_serial_servo_read(3) or 90  # Default to 90 if None
+current_angle_of_servo_1 = arm.Arm_serial_servo_read(1) or 90  
+current_angle_of_servo_2 = arm.Arm_serial_servo_read(2) or 90  
+current_angle_of_servo_3 = arm.Arm_serial_servo_read(3) or 90  
 
 # Define deadband values
 deadband_horizontal = 20
@@ -19,20 +19,25 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Apply Gaussian blur to the frame
+    blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
+    
+    # Convert the blurred frame to HSV color space
+    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
 
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([40, 255, 255])
+    # Define the HSV range for green
+    lower_green = np.array([30, 40, 40])
+    upper_green = np.array([90, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    # Create a binary mask where the green range is white
+    mask = cv2.inRange(hsv, lower_green, upper_green)
 
     # Find contours in the frame
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        # Find the biggest contour
         c = max(contours, key=cv2.contourArea)
-        # Get the coordinates of the center of the contour
         M = cv2.moments(c)
         if M["m00"] != 0:
             cx = int(M["m10"] / M["m00"])
@@ -56,7 +61,7 @@ while True:
                 current_angle_of_servo_1 -= adjustment_angle
                 arm.Arm_serial_servo_write(1, current_angle_of_servo_1, 500)
 
-         # Vertical tracking using two servos
+        # Vertical tracking using two servos
         if cy < 210 - deadband_vertical:
             if current_angle_of_servo_2 < 180:
                 current_angle_of_servo_2 += adjustment_angle
@@ -72,10 +77,11 @@ while True:
                 current_angle_of_servo_3 += adjustment_angle
                 arm.Arm_serial_servo_write(3, current_angle_of_servo_3, 500)
 
-
+    # Display the frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release the camera and close the window
 cap.release()
 cv2.destroyAllWindows()
