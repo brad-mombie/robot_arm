a41f2ab8d8ecd308e6d5407ae1b3ac2f9498b84e
brad-mombie <brad.momberg@gmail.com>
2023-09-29 19:25:43 -0700

    added a reset

--- arm_reset_pos.py
+++ arm_reset_pos.py
@@ -6,7 +6,7 @@ from Arm_Lib import Arm_Device
 arm = Arm_Device()
 
 arm.Arm_serial_servo_write(1, 90, 500)
-arm.Arm_serial_servo_write(2, 90, 500)
+arm.Arm_serial_servo_write(2, 120, 500)
 arm.Arm_serial_servo_write(3, 90, 500)
 arm.Arm_serial_servo_write(4, 90, 500)
 arm.Arm_serial_servo_write(5, 90, 500)
